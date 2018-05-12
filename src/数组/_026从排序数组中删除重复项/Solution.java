f5061277c49d97c9833cde6579e76fd5602b27ee
dealsky <dealsky000@outlook.com>
2018-05-12 22:27:11 +0800

    从排序数组中删除重复项

--- src/数组/_026从排序数组中删除重复项/Solution.java
+++ src/数组/_026从排序数组中删除重复项/Solution.java
@@ -7,5 +7,16 @@ package 数组._026从排序数组中删除重复项;
  * @Date: 2018/4/8 22:00
  */
 public class Solution {
-    
+    public int removeDuplicates(int[] nums) {
+        int sum = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            int preNum = nums[i - 1];
+            if (num != preNum) {
+                nums[sum++] = num;
+            }
+        }
+        return sum;
+    }
 }
