17fc6c9c3855ea63665191b99602336a60feb2b8
dealsky <dealsky000@outlook.com>
2018-05-27 21:20:38 +0800

    字符串

---
+++ src/字符串/_242有效的字母异位词/Solution.java
@@ -0,0 +1,43 @@
+package 字符串._242有效的字母异位词;
+
+// https://leetcode-cn.com/problems/valid-anagram/description/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            if (!map.containsKey(c)) {
+                map.put(c, 1);
+            } else {
+                map.put(c, map.get(c) + 1);
+            }
+        }
+
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)) {
+                return false;
+            } else {
+                Integer num = map.get(c);
+                if (num == 1) {
+                    map.remove(c);
+                } else {
+                    map.put(c, num - 1);
+                }
+            }
+        }
+
+        return map.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String s = "rat";
+        String t = "cat";
+        System.out.println(solution.isAnagram(s, t));
+    }
+}
