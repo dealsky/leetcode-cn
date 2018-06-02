ac9bab40305c9109a009a7b9ca26fbc7a8d0d521
dealsky <dealsky000@outlook.com>
2018-06-02 22:27:42 +0800

    链表

---
+++ src/链表/_019删除链表的倒数第N个节点/Solution.java
@@ -0,0 +1,39 @@
+package 链表._019删除链表的倒数第N个节点;
+
+// https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/description/
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null || n <= 0) {
+            return head;
+        }
+
+        ListNode temp = head;
+        for (int i = 0; i < n; i++) {
+            temp = temp.next;
+        }
+
+        if (temp == null) {
+            return head.next;
+        }
+
+        ListNode preNode = head;
+        while (temp.next != null) {
+            preNode = preNode.next;
+            temp = temp.next;
+        }
+        preNode.next = preNode.next.next;
+
+        return head;
+    }
+}
