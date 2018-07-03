7f282aa0c74e85af052299f813937952285d7bdd
dealsky <dealsky000@outlook.com>
2018-07-03 19:27:14 +0800

    动态规划

--- src/动态规划/_053最大子序和/Solution.java
+++ src/动态规划/_053最大子序和/Solution.java
@@ -2,6 +2,8 @@ package 动态规划._053最大子序和;
 
 // https://leetcode-cn.com/problems/maximum-subarray/description/
 
+import java.util.Arrays;
+
 public class Solution {
     public int maxSubArray(int[] nums) {
         int max = nums[0];
@@ -17,11 +19,43 @@ public class Solution {
         return max;
     }
 
+    // 更为精妙的分治法
+    public int maxSubArray2(int[] nums) {
+        int len = nums.length;
+
+        if (len == 1) {
+            return nums[0];
+        }
+
+        int mid = len / 2;
+
+        int leftMax = maxSubArray2(Arrays.copyOfRange(nums, 0, mid));
+        int rightMax = maxSubArray2(Arrays.copyOfRange(nums, mid, len));
+
+        int midLeftMax = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = mid - 1; i >= 0; i--) {
+            sum += nums[i];
+
+            midLeftMax = Math.max(sum, midLeftMax);
+        }
+
+        int midRightMax = Integer.MIN_VALUE;
+        sum = 0;
+        for (int i = mid; i < len; i++) {
+            sum += nums[i];
+
+            midRightMax = Math.max(sum, midRightMax);
+        }
+
+        return Math.max(Math.max(leftMax, rightMax), midLeftMax + midRightMax);
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        int[] arr = new int[] {-2,-1,-3};
+        int[] arr = new int[]{1};
 
-        System.out.println(solution.maxSubArray(arr));
+        System.out.println(solution.maxSubArray2(arr));
     }
 }
