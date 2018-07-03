3ca6859db2f16c7d8586913d7e7291f1dd79c018
dealsky <dealsky000@outlook.com>
2018-07-03 20:18:47 +0800

    设计问题

---
+++ src/设计问题/_384打乱数组/Solution.java
@@ -0,0 +1,56 @@
+package 设计问题._384打乱数组;
+
+// https://leetcode-cn.com/problems/shuffle-an-array/description/
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Solution {
+
+    private int[] arr;
+    private final Random random = new Random();
+
+
+    public Solution(int[] nums) {
+        arr = nums;
+    }
+
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        return arr;
+    }
+
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        int[] result = Arrays.copyOfRange(arr,0, arr.length);
+
+        for (int i = 1; i < result.length; i++) {
+            int j = randomInt(0, i);
+
+            int temp = result[i];
+            result[i] = result[j];
+            result[j] = temp;
+        }
+
+        return result;
+    }
+
+    public int randomInt(int min, int max) {
+        return random.nextInt(max - min + 1) + min;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+        Solution solution = new Solution(arr);
+
+        for (int i = 0; i < 10 ; i++) {
+            int[] current = solution.shuffle();
+
+            for (int num : current) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
