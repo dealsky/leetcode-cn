946dc8313ca9cce935046432cd66a6436beb0cbb
dealsky <dealsky000@outlook.com>
2018-07-10 13:47:11 +0800

    其他

---
+++ src/其他/_268缺失数字/Solution.java
@@ -0,0 +1,52 @@
+package 其他._268缺失数字;
+
+// https://leetcode-cn.com/problems/missing-number/description/
+
+import sun.java2d.pipe.SolidTextRenderer;
+
+import java.io.Console;
+
+public class Solution {
+    public int missingNumber(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int max = Integer.MIN_VALUE;
+        long sum = 0;
+
+        boolean zeroFlag = false;
+
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
+            }
+
+            if (num == 0) {
+                zeroFlag = true;
+            }
+
+            sum += num;
+        }
+
+        long sum2 = add(max);
+
+        if (sum != sum2) {
+            return (int)Math.abs(sum - sum2);
+        } else {
+            return zeroFlag ? max + 1 : 0;
+        }
+    }
+
+    private long add(int n) {
+        return (int)(((n + 1) / 2.0) * n);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] arr = new int[] {0, 2};
+
+        System.out.println(solution.missingNumber(arr));
+    }
+}
