16cf19f36d32a68d8c44d8135e4cdb729f775973
dealsky <dealsky000@outlook.com>
2018-08-11 21:05:03 +0800

    056合并区间

---
+++ src/排序和搜索/_056合并区间/Solution.java
@@ -0,0 +1,49 @@
+package 排序和搜索._056合并区间;
+
+// https://leetcode-cn.com/problems/merge-intervals/description/
+
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Interval {
+    int start;
+    int end;
+
+    Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+}
+
+public class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        if (intervals.size() == 0) {
+            return new ArrayList<>();
+        }
+
+        intervals.sort(Comparator.comparingInt(o -> o.start));
+
+        List<Interval> result = new ArrayList<>();
+        Interval pre = intervals.get(0);
+        for (int i = 1; i < intervals.size(); i++) {
+            Interval cur = intervals.get(i);
+            if (pre.end >= cur.start) {
+                pre.end = Math.max(pre.end, cur.end);
+            } else {
+                result.add(pre);
+                pre = cur;
+            }
+        }
+
+        result.add(pre);
+
+        return result;
+    }
+}
