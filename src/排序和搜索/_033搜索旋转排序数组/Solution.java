3f30a7e45859216f286ed6ecba56607542e6b331
dealsky <dealsky000@outlook.com>
2018-08-11 21:45:28 +0800

    033搜索旋转排序数组

---
+++ src/排序和搜索/_033搜索旋转排序数组/Solution.java
@@ -0,0 +1,45 @@
+package 排序和搜索._033搜索旋转排序数组;
+
+// https://leetcode-cn.com/problems/search-in-rotated-sorted-array/description/
+
+public class Solution {
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+
+        int len = nums.length;
+        int min = nums[0];
+        int left = 0, right = len - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (target < min) {
+                if (nums[mid] >= min || target > nums[mid]) {
+                    left = mid + 1;
+                } else if (nums[mid] > target) {
+                    right = mid - 1;
+                }
+            } else {
+                if (nums[mid] < min || nums[mid] > target) {
+                    right = mid - 1;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.search(new int[]{1, 3}, 3));
+    }
+}
