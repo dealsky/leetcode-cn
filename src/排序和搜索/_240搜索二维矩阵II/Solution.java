5e61573c0b6259a7e589e2abb05612834d5005d6
dealsky <dealsky000@outlook.com>
2018-08-12 23:31:22 +0800

    240搜索二维矩阵II

--- src/排序和搜索/_240搜索二维矩阵II/Solution.java
+++ src/排序和搜索/_240搜索二维矩阵II/Solution.java
@@ -17,7 +17,21 @@ public class Solution {
             return false;
         }
 
-        for (int i = 0; i < n; i++) {
+        int left = 0, right = n;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (matrix[mid][0] >= target) {
+                if (matrix[mid][0] == target) {
+                    return true;
+                }
+
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        for (int i = 0; i < left; i++) {
             if (matrix[i][0] <= target) {
                 if (binarySearch(matrix[i], 0, m - 1, target)) {
                     return true;
@@ -41,5 +55,39 @@ public class Solution {
         }
 
         return false;
+    }
+
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        int n = matrix.length;
+        if (n == 0) {
+            return false;
+        }
+        int m = matrix[0].length;
+        if (m == 0) {
+            return false;
+        }
+
+        if (target < matrix[0][0] || target > matrix[n - 1][m - 1]) {
+            return false;
+        }
+
+        int i = n - 1, j = 0;
+        while (i >= 0 && j < m) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                i--;
+            } else {
+                j++;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.searchMatrix2(new int[][]{{-5}}, -5));
     }
 }
