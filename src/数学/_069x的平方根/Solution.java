56d71dcfe0aba91026a1c4d78791e663d0ed6be2
dealsky <dealsky000@outlook.com>
2018-08-18 19:06:56 +0800

    069x的平方根

---
+++ src/数学/_069x的平方根/Solution.java
@@ -0,0 +1,41 @@
+package 数学._069x的平方根;
+
+// https://leetcode-cn.com/problems/sqrtx/description/
+
+public class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+
+        if (x < 4) {
+            return 1;
+        }
+
+        return binarySearch(2, x / 2, x);
+    }
+
+    private int binarySearch(int left, int right, long target) {
+        if (left == right) {
+            return left;
+        }
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if ((long) mid * mid >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left * left == target ? left : left - 1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.mySqrt(5));
+    }
+}
