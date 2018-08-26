10eb6c8573453a5b975d3709244b12cbf2652d8f
dealsky <dealsky000@outlook.com>
2018-08-26 19:08:05 +0800

    233数字1的个数

---
+++ src/数学/_233数字1的个数/Solution.java
@@ -0,0 +1,20 @@
+package 数学._233数字1的个数;
+
+// https://leetcode-cn.com/problems/number-of-digit-one/description/
+
+public class Solution {
+    public int countDigitOne(int n) {
+        int sum = 0;
+
+        for (long i = 1; i <= n; i *= 10) {
+            long left = n / i, right = n % i;
+
+            sum += (left + 8) / 10 * i;
+            if (left % 10 == 1) {
+                sum += right + 1;
+            }
+        }
+
+        return sum;
+    }
+}
