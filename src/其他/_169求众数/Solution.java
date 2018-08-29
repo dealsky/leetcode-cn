96009732fe60432eac7bbfcf628a97c1693fde80
dealsky <dealsky000@outlook.com>
2018-08-29 20:50:23 +0800

    169求众数

---
+++ src/其他/_169求众数/Solution.java
@@ -0,0 +1,24 @@
+package 其他._169求众数;
+
+// https://leetcode-cn.com/problems/majority-element/description/
+
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int result = -1, count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                result = num;
+                count++;
+            } else {
+                if (num == result) {
+                    count++;
+                } else {
+                    count--;
+                }
+            }
+        }
+
+        return result;
+    }
+}
