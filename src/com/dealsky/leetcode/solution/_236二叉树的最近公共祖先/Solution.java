8416644645109476c6277cda8a0cbed6eab9179f
dealsky <dealsky000@outlook.com>
2019-02-28 21:40:12 +0800

    二叉树的最近公共祖先

---
+++ src/com/dealsky/leetcode/solution/_236二叉树的最近公共祖先/Solution.java
@@ -0,0 +1,28 @@
+package com.dealsky.leetcode.solution._236二叉树的最近公共祖先;
+
+import com.dealsky.leetcode.util.TreeNode;
+
+
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else if (right != null) {
+            return right;
+        } else {
+            return null;
+        }
+    }
+}
